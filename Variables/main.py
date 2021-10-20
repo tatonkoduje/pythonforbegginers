a4a7de61b6da4818f074c4d64a7f9e790e91d71c
Arkadiusz Opozda <aopozda@atsi.zab>
2021-10-20 23:07:10 +0200

    Chapter 2: Variables

---
+++ Variables/main.py
@@ -0,0 +1,26 @@
+myString = "Witaj na kursie dla początkujących"
+name = "Marek"
+print(myString)
+print("Moje imię to: " + name)
+print(myString + " " + name)
+
+age = 18
+print(age)
+print("Mam " + str(age) + " lat")
+print(type(age))
+
+
+weight = 75.5
+print("Moja waga to " + str(weight) + " kg")
+
+hair = 0x000000  # int('920310', 16)
+
+isBlack = True
+print("Czy kolor włosów jest czarny?: " + str(isBlack))
+
+name1 = name2 = name3 = "Stefan"
+print(name1)
+print(name2)
+print(name3)
+
+
