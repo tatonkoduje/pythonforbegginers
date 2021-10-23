df3db5236fe0ace4669b914e581c48672729093b
Taton Koduje <taton.koduje@gmail.com>
2021-10-23 19:25:08 +0200

    Chapter 8: String Methods

---
+++ StringMethods/main.py
@@ -0,0 +1,18 @@
+name = "marek"
+surname = "Kowalski"
+age = "21"
+
+print(name)
+print(len(name))
+print(name.capitalize())
+print(surname.lower())
+print(surname.upper())
+print(surname.find('w'))
+print(name.find('w'))
+print(surname.index('s'))
+print(age.isalpha())
+print(surname.isalpha())
+print(surname.count('k'))
+print(name.islower())
+print(name.replace('m', 'd'))
+print(name.split('a'))
