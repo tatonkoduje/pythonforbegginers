10de344fac061e14555196193d33fc6ffffc2dbb
Taton Koduje <taton.koduje@gmail.com>
2021-10-26 21:28:51 +0200

    update

--- Tuples/main.py
+++ Tuples/main.py
@@ -4,8 +4,20 @@ print(person)
 print(person.count("Marek"))
 print(person.index("m"))
 print(person[4])
+print(person[-1])
+print(person[2:5])
 
 print("------------------------")
 
 for element in person:
     print(element)
+
+print("------------------------")
+
+tuple1 = (1, 2, 3)
+tuple2 = ("a", "b", "c")
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+tuple4 = tuple1 * 3
+print(tuple4)
