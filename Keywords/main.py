9f168060a3b14cf6b87f0085720db3dfc3627760
Taton Koduje <taton.koduje@gmail.com>
2021-11-06 12:18:53 +0100

    Chapter 21: Keyword arguments

---
+++ Keywords/main.py
@@ -0,0 +1,11 @@
+def new_person(name, surname, age, sex):
+    print("name: ", name)
+    print("surname: ", surname)
+    print("age: ", age)
+    print("sex: ", sex)
+    print("-----------------------------------")
+
+
+new_person("Marek", "Kowalski", 18, "m")
+new_person("m", "Marek", "Kowalski", 18)
+new_person(sex="m", name="Marek", surname="Kowalski", age=18)
