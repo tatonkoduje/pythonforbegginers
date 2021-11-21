f20af7b81fa07d928de1c3d1ddd406cde85e141a
Taton Koduje <taton.koduje@gmail.com>
2021-11-21 22:13:25 +0100

    update

--- Canvas/main.py
+++ Canvas/main.py
@@ -3,6 +3,8 @@ from tkinter import *
 window = Tk()
 
 canvas = Canvas(window)
+canvas.pack()
+
 canvas.config(height=500, width=500, bg='#CCCCCC')
 
 canvas.create_line(10, 10, 300, 300, fill='green', width=3)
@@ -12,7 +14,6 @@ canvas.create_rectangle(100, 100, 200, 200, fill='blue')
 
 points = [300, 50, 400, 400, 200, 400]
 canvas.create_polygon(points, fill='yellow', outline='pink', width=10)
-canvas.pack()
 
 canvas.create_arc(0, 0, 500, 500, fill='brown', style=PIESLICE, start=180, extent=180)
 
