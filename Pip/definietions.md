0fcf21e076a667a01b31060ee455f522fb7ffd65
Taton Koduje <taton.koduje@gmail.com>
2021-11-27 14:38:55 +0100

    Chapter 76: Pip

---
+++ Pip/definietions.md
@@ -0,0 +1,4 @@
+## pip
+
+**pip** - jest to menadżer paczek i modułów. Za jego pomocą ściągamy i instalujemy dodatkowe paczki
+Dostępne od wersji Python 3.4
---
+++ Pip/main.py
@@ -0,0 +1,10 @@
+# Dodać zmienną środowiskową do systemu (...\Python310\Scripts)
+
+# run cmd
+# pip --version
+# pip install --upgrade pip
+# pip list
+# pip list --outdated
+# pip install pandas
+
+# adres: pip.org
