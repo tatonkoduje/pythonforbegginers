8086a4edf7dea474c4baf27ca5e6d4816b75c9c3
tatonkoduje <92830444+tatonkoduje@users.noreply.github.com>
2021-11-28 19:42:33 +0100

    Chapter 20: Return statement (#23)

---
+++ Return/main.py
@@ -0,0 +1,17 @@
+def get_name():
+    return "Marek"
+
+
+get_name()
+print(get_name())
+
+name = get_name()
+print(name)
+
+
+def sum(a, b):
+    return a + b
+
+
+result = sum(12, 32)
+print(result)
