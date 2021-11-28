be1ccb128bfaf7c779b6a33049386f07efc3583d
tatonkoduje <92830444+tatonkoduje@users.noreply.github.com>
2021-11-28 16:48:00 +0100

    Chapter 7: Math (#7)

---
+++ Math/main.py
@@ -0,0 +1,27 @@
+import math
+
+print("Stałe")
+print("--------------------------")
+print(math.inf)
+print(math.nan)
+print(math.pi)
+print("--------------------------")
+
+pi = math.pi
+a = 1
+b = 2
+c = 30
+d = -9
+e = 7.72
+
+print("Funkcje")
+print("--------------------------")
+print("ceil pi: ", math.ceil(pi))
+print("floor pi: ", math.floor(pi))
+print("round pi: ", round(pi))
+print("round e: ", round(e))
+print("math.pow b^2: ", math.pow(b, 2))
+print("pow b^2: ", pow(b, 2))
+
+print("abs d: ", abs(d))
+print("sqrt i abs d: ", math.sqrt(abs(d)))
