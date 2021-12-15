da400803c8454e0c3893e9e431bce2ec624feb80
tatonkoduje <92830444+tatonkoduje@users.noreply.github.com>
2021-12-15 15:24:13 +0100

    Chapter 49: Zip (#53)

---
+++ Zip/definitions.md
@@ -0,0 +1,4 @@
+## zip
+
+**zip** - to funkcja która agreguje elementy z więcej niż jednego obiektu typu lista, set, tuple itd.
+Zwraca obiekt **zip** ze sparowanymi elementami jako tuple.
---
+++ Zip/main.py
@@ -0,0 +1,11 @@
+brands = ["polonez", "fiat126p", "traband", "żuk", "syrena"]
+colors = ("czerwony", "niebieski", "biały", "czarny", "żółty")
+
+cars = list(zip(brands, colors))
+print(cars)
+
+
+years = [1972, 1993, 1969, 1988, 1976]
+
+cars2 = list(zip(brands, colors, years))
+print(cars2)
