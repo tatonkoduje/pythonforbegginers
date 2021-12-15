7e1442d58fdb72353851e0b823b7145d72f6976d
tatonkoduje <92830444+tatonkoduje@users.noreply.github.com>
2021-12-15 15:58:31 +0100

    Chapter 78: Cmd (#82)

---
+++ Cmd/main.py
@@ -0,0 +1,11 @@
+from time import *
+
+print("Aktualny czas")
+
+current_time = strftime("%H:%M:%S")
+print(current_time)
+
+name = input("Jak masz na imię? ")
+print("Witaj " + name + " !!!")
+
+# w zmiennych środowiskowych - PATH - przesuń w dół WindowsApp
